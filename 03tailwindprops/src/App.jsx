fc0323f1f895639dd6e07c84a7985a8143d187ce
vikasya7 <vikasyaa19@gmail.com>
2025-10-24 11:40:45 +0530

    feat: initialize Tailwind CSS project with React and Vite
    - Added package.json with dependencies for React, Tailwind CSS, and Vite.
    - Created Vite configuration file for Tailwind CSS integration.
    - Added main application structure with App component and styles.
    - Included SVG assets for React and Vite logos.
    - Set up global styles with Tailwind CSS and custom CSS.
    - Implemented responsive design and hover effects in CSS.

---
+++ 03tailwindprops/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import React from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+        <h1>Tailwind props</h1>
+     </>
+  )
+}
+
+export default App
